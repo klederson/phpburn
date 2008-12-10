d4c7a16cdfa4e60e8573c681743ee83ab320097d
Klederson Bueno <klederson@klederson.com>
2008-12-10 23:43:53 +0100

    Minor bug in tokens translation

--- app/libs/Extras/Views/Views.php
+++ app/libs/Extras/Views/Views.php
@@ -53,12 +53,17 @@ abstract class PhpBURN_Views implements IViews {
 			foreach($out as $index => $arrContent) {
 				$pieces = explode(':',$arrContent[1]);
 				for($i = 0; $i < count($pieces); $i++) {
-					$stringArray .= "[$pieces[$i]]";
+					$stringArray .= "['$pieces[$i]']";
 				}
 				eval("\$_tmpValue = \$tokens$stringArray;");
+				
+				
+				if($_tmpValue != null) {
 								
-				$contentValue = $lang == null ? $_tmpValue : self::translate($_tmpValue, $lang);
-				$template = str_replace("[#$arrContent[1]#]",$contentValue,$template);
+					$contentValue = $lang == null ? $_tmpValue : self::translate($_tmpValue, $lang);
+					$template = str_replace("[#$arrContent[1]#]",$contentValue,$template);
+				
+				}
 				unset($stringArray,$_tmpValue,$contentValue);
 			}
 		} else {
