0ffbd1d1738a2efe1db68aa66b0dcf6c7e5a6bb5
Klederson Bueno <klederson@klederson.com>
2009-07-13 14:07:01 -0300

    Now getLink and getRelationship works like find() and returns the result amount

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -507,17 +507,15 @@ abstract class PhpBURN_Core implements IPhpBurn {
 					exit;
 				}
 				
-				//return $this->$fieldInfo['alias']->fetch();
+				return $amount;
 			break;
 			case self::ONE_TO_MANY:
 //				Looking for ONE TO MANY relationship
 				
 //				Define WHERE based on relationship fields
-				$this->$fieldInfo['alias']->swhere($fieldInfo['relKey'],'=',$this->$fieldInfo['thisKey']);
-				
-				$amount = $this->$fieldInfo['alias']->find();				
+				$this->$fieldInfo['alias']->swhere($fieldInfo['relKey'],'=',$this->$fieldInfo['thisKey']);		
 								
-				//return $this->$fieldInfo['alias']->fetch();				
+				return $this->$fieldInfo['alias']->find();			
 			break;
 			case self::MANY_TO_MANY:
 //				Looking for MANY TO MANY relationship
@@ -525,7 +523,7 @@ abstract class PhpBURN_Core implements IPhpBurn {
 				$this->$fieldInfo['alias']->join($fieldInfo['relTable'],$fieldInfo['thisKey'],$fieldInfo['relKey']);
 				$this->$fieldInfo['alias']->join($fieldInfo['relTable'],$fieldInfo['outKey'],$fieldInfo['relOutKey']);
 				
-				//return $this->$fieldInfo['alias']->find();
+				return $this->$fieldInfo['alias']->find();
 			break;
 		}
 	}
