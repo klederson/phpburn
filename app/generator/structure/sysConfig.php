6d6ad87cb34c6b14296f2089b9d2d0069bcbb4c0
Klederson Bueno <klederson@klederson.com>
2010-11-26 19:09:47 -0200

    Added Spices Settings to Structure

--- app/generator/structure/sysConfig.php
+++ app/generator/structure/sysConfig.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * System Configuration File
- * 
- * This file contains the main constants and variables ( as well some functions ) 
+ *
+ * This file contains the main constants and variables ( as well some functions )
  * to make the system work all right and plug'n'play.
  */
 
@@ -34,6 +34,7 @@ define('SYS_APPLICATION_PATH', SYS_BASE_PATH . DS . 'system');
 define('SYS_MODEL_PATH',SYS_APPLICATION_PATH . DS . 'model' . DS,true);
 define('SYS_VIEW_PATH',SYS_APPLICATION_PATH . DS . 'views' . DS,true);
 define('SYS_CONTROLLER_PATH',SYS_APPLICATION_PATH . DS . 'controllers' . DS,true);
+define('SYS_SPICES_PATH', SYS_APPLICATION_PATH . DS . 'config' . DS . 'spices' . DS);
 
 define('SYS_CSS_PATH',SYS_BASE_PATH . DS . 'public' . DS . 'css' ,true);
 define('SYS_IMAGE_PATH',SYS_BASE_PATH . DS . 'public' . DS . 'images',true);
@@ -47,6 +48,8 @@ define('SYS_FILE_PATH',SYS_BASE_PATH . DS . 'public' . DS . 'files',true);
 define('SYS_MODEL_EXT', '.php',true);
 define('SYS_VIEW_EXT', 'php',true);
 define('SYS_CONTROLLER_EXT', 'php',true);
+define('SYS_SPICES_EXT','spices.php',true);
+
 
 ################################
 # Infos
@@ -55,6 +58,7 @@ define('PHPBURN_VERSION','0.9b',true);
 define('PHPBURN_WEBSITE','http://www.phpburn.com/',true);
 define('PHPBURN_REPOSITORY_TYPE','git',true);
 define('PHPBURN_REPOSITORY','http://github.com/klederson/phpburn/tree/master',true);
+
 
 ##########################################
 # EXTRAS
