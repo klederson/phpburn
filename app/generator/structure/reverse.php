98c029cdea67b6bd21c80f8046beffe56c20a6c2
Klederson Bueno <klederson@klederson.com>
2010-12-07 16:51:01 -0200

    Added CONSOLE debug message mode ON for reverse.php

--- app/generator/structure/reverse.php
+++ app/generator/structure/reverse.php
@@ -2,13 +2,19 @@
 ################################
 # Hooks
 ################################
-define('SYS_USE_FIREPHP',true,true);
+define('SYS_USE_FIREPHP',false,true);
 
 ################################
 # Including required files
 ################################
 require_once('app/phpBurn.php');
 require_once('config.php');
+
+################################
+# Start PhpBURN needed resources
+################################
+PhpBURN::enableAutoload();
+PhpBURN_Message::setMode(PhpBURN_Message::CONSOLE);
 
 //Migrations tool
 PhpBURN::load('Migrations.Reverse');
