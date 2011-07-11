a8cdd8f437640c8eafedda3afa94cb41b79e5a99
Klederson Bueno <klederson@klederson.com>
2011-07-10 23:09:02 -0300

    Thinking about the future now all 'lazy' translation can work with old PhpBURN translate system

--- app/libs/Tools/Views/phptal.php
+++ app/libs/Tools/Views/phptal.php
@@ -1,46 +1,49 @@
 <?php
+
 PhpBURN::load('Tools.Views.IProcessView');
 PhpBURN::load('Addons.PHPTAL.PHPTAL');
 
 class phptal_PhpBURN_ViewProcess implements IProcessView {
 
-    /**
-     * This method is used to process the data into the view and than return it to the main method that will handle what to do.
-     * It also uses buffer to handle that content.
-     *
-     * @author Klederson Bueno <klederson@klederson.com>
-     * @version 0.1a
-     *
-     * @param String $___phpBurnFilePath
-     * @param Array $__phpBurnData
-     * @return String
-     */
-    public function processViewData($___phpBurnFilePath, $__phpBurnData) {
-        $tpl = new PHPTAL($___phpBurnFilePath);
+  /**
+   * This method is used to process the data into the view and than return it to the main method that will handle what to do.
+   * It also uses buffer to handle that content.
+   *
+   * @author Klederson Bueno <klederson@klederson.com>
+   * @version 0.1a
+   *
+   * @param String $___phpBurnFilePath
+   * @param Array $__phpBurnData
+   * @return String
+   */
+  public function processViewData($___phpBurnFilePath, $__phpBurnData) {
+    $tpl = new PHPTAL($___phpBurnFilePath);
 
-        $tpl->setOutputMode(PHPTAL::HTML5);
-        
-        foreach($__phpBurnData as $index => $value) {
-            $tpl->$index = $value;
-        }
+    $tpl->setOutputMode(PHPTAL::HTML5);
 
-        ob_start();
+    foreach ($__phpBurnData as $index => $value) {
+      if (is_string($value))
+        $value = PhpBURN_Views::lazyTranslate($value);
 
-            try {
-                echo $tpl->execute();
-            }
-            catch (Exception $e){
-                echo $e;
-            }
+      $tpl->$index = $value;
+    }
 
-            $___phpBurnBufferStored = ob_get_contents();
+    ob_start();
+
+    try {
+      echo $tpl->execute();
+    } catch (Exception $e) {
+      echo $e;
+    }
+
+    $___phpBurnBufferStored = ob_get_contents();
 //
 //        //Cleaning the buffer for new sessions
-        ob_clean();
+    ob_clean();
 
-        return $___phpBurnBufferStored;            
+    return $___phpBurnBufferStored;
+  }
 
-    }
 }
 
 ?>
