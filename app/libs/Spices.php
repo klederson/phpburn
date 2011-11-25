efe96d4465d288f0433c471e088684679b3f0a29
Klederson Bueno <klederson@klederson.com>
2011-11-25 15:42:46 -0200

    BETA Spices under development

--- app/libs/Spices.php
+++ app/libs/Spices.php
@@ -1,16 +1,20 @@
 <?php
+
 namespace PhpBURN\Spices;
 
 class Spices {
-    public static function loadSpice($name = null) {
-        $name = $name == null ? '*.' : sprintf('%s.', $name);
-        $loadPath = sprintf("%s%s%s",SYS_SPICES_PATH, '*.', SYS_SPICES_EXT);
 
-        foreach ( glob( $loadPath ) as $filename) {
-            require_once($filename);
-        }
-        
-        unset($loadPath);
+  public static function loadSpice($name = null) {
+    $name = $name == null ? '*.' : sprintf('%s.', $name);
+    $loadPath = sprintf("%s%s%s", SYS_SPICES_PATH, $name, SYS_SPICES_EXT);
+
+    foreach (glob($loadPath) as $filename) {
+      require_once($filename);
     }
+
+    unset($loadPath);
+  }
+
 }
+
 ?>
